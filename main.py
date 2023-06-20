984b539faf482ffeb38583cb07fb351dbce85049
Ilshat <illliat@mail.ru>
2023-06-20 21:43:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from model.note import Note
+
+n = Note("qwe")
+m = Note(123)
+print(n.id)
+print(m.id)
---
+++ model/note.py
@@ -0,0 +1,6 @@
+class Note:
+
+    def __init__(self, idn, header):
+        self.__idn = idn
+        self.__header = header
+
